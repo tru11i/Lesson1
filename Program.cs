94a878e1e3d6b517477963e9f75c69fa4b5cb257
Artur <ricciantony0@gmail.com>
2023-12-26 22:44:59 +0600

    F

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// Task1
+
+// Console.WriteLine("Vvedite chisla");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = Convert.ToInt32(Console.ReadLine());
+// if(A > B)
+
+// {
+//     Console.Write("Max = ");
+//     Console.WriteLine(A);
+//     Console.Write("Min = ");
+//     Console.WriteLine(B);
+// }
+// else
+// {
+//     Console.Write("Max = ");
+//     Console.WriteLine(B);
+//     Console.Write("Min = ");
+//     Console.WriteLine(A);
+// }
+
+// Task2
+
+// Console.WriteLine("Vvedite chisla");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = Convert.ToInt32(Console.ReadLine());
+// int C = Convert.ToInt32(Console.ReadLine());
+
+// int max = A;
+
+// if (A > max) max = A;
+// if (B > max) max = B;
+// if (C > max) max = C;
+
+// Console.WriteLine("Max = ");
+// Console.WriteLine(max);
+
+// Task3
+
+// Console.WriteLine("Vvedite chisla");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// if (A % 2 == 0)
+// {
+//     Console.WriteLine("Da");
+// }
+// else
+// {
+//     Console.WriteLine("Net");
+// }
+
+// Task4
+
+// Console.WriteLine("Vvedite chislo");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;
+
+// while (i < A)
+// {
+//     if (i % 2 ==0)
+//         {
+//             Console.WriteLine(i);
+//         }
+//     i += 1;
+// }
